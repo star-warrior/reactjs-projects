4e724d0cee46a82f35fcc5b251541b60086c0203
Jay Mehta <69752445+star-warrior@users.noreply.github.com>
2025-06-25 11:14:37 +0530

    React Router Compleeted

---
+++ react-router-prac/src/components/Pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "../Home/Home";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
